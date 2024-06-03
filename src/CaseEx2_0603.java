52762c260f9ebec65de4f9003dfcc5854a023059
kongrtan1 <kongrtan92@naver.com>
2024-06-03 15:32:00 +0900

    init

---
+++ src/CaseEx2_0603.java
@@ -0,0 +1,24 @@
+public class CaseEx2_0603 {
+
+  public static void main(String[] args) {
+
+    String operator = "+";
+    int num1 = 2;
+    int num2 = 4;
+
+    switch (operator) {
+      case "+":
+        System.out.println(num1 + operator + num2);
+        break;
+      case "-":
+        System.out.println(num1 + operator + num2);
+        break;
+      case "*":
+        System.out.println(num1 + operator + num2);
+        break;
+      case "/":
+        System.out.println(num1 + operator + num2);
+        break;
+    }
+  }
+}
