4e786f55ea2f0254e131c6f99c592a6d707d211c
kongrtan1 <kongrtan92@naver.com>
2024-06-03 14:15:59 +0900

    init

---
+++ src/if_0603.java
@@ -0,0 +1,18 @@
+public class if_0603 {
+
+  public static void main(String[] args) {
+    int number = 10;
+
+    if (number > 10){
+      System.out.println( "number는 10볻 ㅏ큽니다.");
+    }else if(number < 10){
+
+      System.out.println("number는 10보다 작습니다.");
+      if (number > 5){
+        System.out.println("number는 5보다 큽니다.");
+      }
+    } else {
+
+    }
+  }
+}
