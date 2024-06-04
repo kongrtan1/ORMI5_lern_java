418be7bb38242b6400dbd08b41676bf9d919f42f
kongrtan1 <kongrtan92@naver.com>
2024-06-04 18:29:46 +0900

    init

---
+++ src/DailyQuiz2_0604.java
@@ -0,0 +1,9 @@
+public class DailyQuiz2_0604 {
+  public static void main(String[] args) {
+    for (int i = 9; i >= 2; i--) {
+      for (int j = 9; j >= 1; j--) {
+        System.out.println(i + " X " + j + " = " + i * j);
+      }
+    }
+  }
+}
---
+++ src/DailyQuiz_0604.java
@@ -0,0 +1,12 @@
+public class DailyQuiz_0604 {
+    public static void main(String[] args){
+        int count = 1;
+        while (true){
+            if (count>=10000){
+                break;
+            }
+            count++;
+      System.out.println(count);
+        }
+    }
+}
